7c47cd4ff6752415f47d67f48d0346e3802bf49c
Javad Hosseini <sr.javadhosseini@gmail.com>
2023-07-31 17:12:54 +0330

    adding name to the createSideColor utility

--- utils/createSideColor.js
+++ utils/createSideColor.js
@@ -1,12 +1,13 @@
 let CreateSideColor = id => {
     let out = {}
 
-    switch ( id ) {
+    switch (id) {
         // Citizen
         case "630a35978c198e0d655c8adf":
             out = {
                 background: '#549088',
-                color:      '#FFFFFF'
+                color: '#FFFFFF',
+                name: 'شهروند'
             }
             break
 
@@ -14,7 +15,8 @@ let CreateSideColor = id => {
         case "630a359e8c198e0d655c8ae0":
             out = {
                 background: '#cb5240',
-                color:      '#FFFFFF'
+                color: '#FFFFFF',
+                name: 'مافیا'
             }
             break
 
@@ -22,7 +24,8 @@ let CreateSideColor = id => {
         case "630a35a38c198e0d655c8ae1":
             out = {
                 background: '#ffde43',
-                color:      '#333333'
+                color: '#333333',
+                name: 'مستقل'
             }
             break
 
@@ -30,7 +33,8 @@ let CreateSideColor = id => {
         case "630a3773696bb14037bdec19":
             out = {
                 background: '#b4b8b9',
-                color:      '#333333'
+                color: '#333333',
+                name: 'لشگر سیاه'
             }
             break
 
@@ -38,7 +42,8 @@ let CreateSideColor = id => {
         case "630a383d696bb14037bdec1a":
             out = {
                 background: '#83bb70',
-                color:      '#333333'
+                color: '#333333',
+                name: 'کرونا'
             }
             break
 
@@ -46,15 +51,26 @@ let CreateSideColor = id => {
         case "630a389a696bb14037bdec1b":
             out = {
                 background: '#1e1e1e',
-                color:      '#FFFFFF'
+                color: '#FFFFFF',
+                name: 'مجهول'
             }
             break
 
         // Unknown
+        case "630a38c1696bb14037bdec1c":
+            out = {
+                background: '#E5E5E5',
+                color: '#333333',
+                name: 'مجهول خاص'
+            }
+            break
+
+        // Twin
         case "630a3926696bb14037bdec1d":
             out = {
                 background: '#E5E5E5',
-                color:      '#333333'
+                color: '#333333',
+                name: 'همزاد'
             }
             break
     }
