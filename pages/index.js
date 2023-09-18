a5d02994357be964e16a4c9c416cf4c9c9a35fcf
Javad Hosseini <sr.javadhosseini@gmail.com>
2023-09-18 23:52:26 +0330

    creating Add to Home Screen modal

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ import Image from "next/future/image";
 import logo from "../public/logo-white.png";
 import Link from 'next/link';
 import Circular from '../components/Circular';
+import InstallationModal from '../components/InstallationModal';
 
 
 // import { getuser } from '../redux/actions';
@@ -15,12 +16,25 @@ const Index = props => {
     const router = useRouter()
 
     const { globalUser } = useSelector(state => state.userReducer)
+    const [showModal, setShowModal] = useState(false);
 
     useEffect(() => {
         if (globalUser && globalUser.isLoggedIn) {
             router.push('/mafia/profile')
         }
     }, [globalUser])
+
+    useEffect(() => {
+        const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent);
+
+        if (isIOS && !window.navigator.standalone) {
+            setShowModal(true);
+        }
+    }, [])
+
+    const handleInstallClick = () => {
+        setShowModal(false)
+    };
 
     return (
         (globalUser && globalUser.isLoggedIn) ?
@@ -41,6 +55,10 @@ const Index = props => {
                     </Link>
                     کلیک کنید</span>
                 <div className={styles.footer}>From IA</div>
+
+                {showModal && (
+                    <InstallationModal onInstallClick={handleInstallClick} />
+                )}
             </div>
 
     )
