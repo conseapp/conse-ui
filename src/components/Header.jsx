ac4883cf53ef19478a01f4562efd2eb59137d5b3
JavadH0sseini <sr.javadhosseini@gmail.com>
2024-02-17 19:33:21 +0330

    fixing an bug in header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -56,6 +56,7 @@ const Header = () => {
         },
         onError: (error) => {
           toast.error('خطایی در هنگام افزایش موجودی پیش آمده')
+          setButtonDisabled(false)
         },
       })
 
