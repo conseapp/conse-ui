4ae35dfa3e2be7eb13ab4584814e27bf87893dad
JavadH0sseini <sr.javadhosseini@gmail.com>
2024-02-24 04:15:50 +0330

    putting the raw body of ckeditor content from blog post instead of extracted body

--- src/pages/jamnet/SinglePost.jsx
+++ src/pages/jamnet/SinglePost.jsx
@@ -23,8 +23,10 @@ const SinglePost = () => {
                         <img src={singlePost?.image} className="w-full max-w-2xl aspect-video bg-gray-dark rounded-2xl" />
                         <div className="flex flex-col gap-3 px-2">
                             <h2 className="text-2xl">{singlePost?.title}</h2>
-                            <p className="text-right leading-loose">{singlePost?.extracted_body}</p>
+                            {/* <p className="text-right leading-loose">{singlePost?.extracted_body}</p> */}
                             {/* <div dangerouslySetInnerHTML={{__html:singlePost?.body}}></div> */}
+                            <div className="text-right leading-loose">{singlePost?.body}</div>
+
                         </div>
                         <div className="flex w-full justify-between p-2">
                             <span className="text-black bg-secondary rounded-lg pt-1 px-2">
