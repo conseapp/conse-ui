c95d1f2c5479234b15275e06ca8bce30fd948d6e
JavadH0sseini <sr.javadhosseini@gmail.com>
2024-02-26 15:55:42 +0330

    showing landing to user only if he is not logged in

--- src/pages/landing/Landing.jsx
+++ src/pages/landing/Landing.jsx
@@ -4,9 +4,12 @@ import homeSVG from './assets/svg/home.svg'
 import phoneSVG from './assets/svg/phone.svg'
 import personSVG from './assets/svg/person.svg'
 import styles from './style.module.scss';
-import { Link } from 'react-router-dom'
+import { Link, Navigate, useLocation } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 const Landing = () => {
+    const globalUser = useSelector(state => state.userReducer)
+    const locaion = useLocation()
     const interBubbleRef = useRef(null);
 
     useEffect(() => {
@@ -48,10 +51,11 @@ const Landing = () => {
 
 
     return (
-        <div className='bg-black w-full h-screen'>
-            <nav className="absolute bg-transparent z-50 w-full mt-[3vw]">
-                <div className="w-[90vw] lg:w-[80vw] 2xl:w-[70vw] mx-auto flex py-4 justify-between">
-                    {/* <ul className="flex items-center justify-center text-white gap-10 box-border">
+        !globalUser.isLoggedIn ?
+            <div className='bg-black w-full h-screen'>
+                <nav className="absolute bg-transparent z-50 w-full mt-[3vw]">
+                    <div className="w-[90vw] lg:w-[80vw] 2xl:w-[70vw] mx-auto flex py-4 justify-between">
+                        {/* <ul className="flex items-center justify-center text-white gap-10 box-border">
                         <li className="">
                             <img
                                 className="m-auto"
@@ -61,7 +65,7 @@ const Landing = () => {
                             />
                         </li>
                     </ul> */}
-                    {/* <div>
+                        {/* <div>
                         <ul className="flex gap-3 md:gap-5">
                             <li className="nav-button2">
                                 <a href="">
@@ -80,16 +84,16 @@ const Landing = () => {
                             </li>
                         </ul>
                     </div> */}
-                </div>
-            </nav>
-            <div className="absolute w-[100vw] h-[100vh] flex flex-col justify-center items-center z-40 gap-16">
-                <div className="flex flex-col items-center text-white justify-center">
-                    <p className="text-[6rem] md:text-[10rem] mb-[-18px] sm:mb-[-26px]">
-                        جمشید
-                    </p>
-                    <p className="text-[1.5rem] md:text-[2.5rem]">پلتفرم بازی های دورهمی</p>
-                </div>
-                {/* <div className="border-[3px] border-[#FFFFFF90] rounded-2xl flex pt-1 mt-5 text-white backdrop-blur">
+                    </div>
+                </nav>
+                <div className="absolute w-[100vw] h-[100vh] flex flex-col justify-center items-center z-40 gap-16">
+                    <div className="flex flex-col items-center text-white justify-center">
+                        <p className="text-[6rem] md:text-[10rem] mb-[-18px] sm:mb-[-26px]">
+                            جمشید
+                        </p>
+                        <p className="text-[1.5rem] md:text-[2.5rem]">پلتفرم بازی های دورهمی</p>
+                    </div>
+                    {/* <div className="border-[3px] border-[#FFFFFF90] rounded-2xl flex pt-1 mt-5 text-white backdrop-blur">
                     <div className="text-2xl px-10 pb-3 hover:font-bold hover:translate-y-[-2px] transition-all">
                         <Link to={'/mafia/profile'}>ورود</Link>
                     </div>
@@ -98,42 +102,43 @@ const Landing = () => {
                         <Link to={'/mafia/profile'}>ثبت نام</Link>
                     </div>
                 </div> */}
-                <div className='flex flex-col text-2xl items-center gap-3'>
-                    <div className='hover:font-bold hover:translate-y-[-2px] transition-all'>
-                        <Link to={'/mafia/profile'}>ورود</Link>
+                    <div className='flex flex-col text-2xl items-center gap-3'>
+                        <div className='hover:font-bold hover:translate-y-[-2px] transition-all'>
+                            <Link to={'/mafia/profile'}>ورود</Link>
+                        </div>
+                        <div className="w-full h-[60%] border-2 my-auto rounded border-[#FFFFFF90]" />
+                        <div className='hover:font-bold hover:translate-y-[-2px] transition-all'>
+                            <Link to={'/mafia/profile'}>ثبت نام</Link>
+                        </div>
                     </div>
-                    <div className="w-full h-[60%] border-2 my-auto rounded border-[#FFFFFF90]" />
-                    <div className='hover:font-bold hover:translate-y-[-2px] transition-all'>
-                        <Link to={'/mafia/profile'}>ثبت نام</Link>
+                </div>
+                <div className={styles.gradient_bg}>
+                    <svg xmlns="http://www.w3.org/2000/svg">
+                        <defs>
+                            <filter id="goo">
+                                <feGaussianBlur in="SourceGraphic" stdDeviation={10} result="blur" />
+                                <feColorMatrix
+                                    in="blur"
+                                    mode="matrix"
+                                    values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -8"
+                                    result="goo"
+                                />
+                                <feBlend in="SourceGraphic" in2="goo" />
+                            </filter>
+                        </defs>
+                    </svg>
+                    <div className={styles.gradients_container}>
+                        <div className={styles.g1} />
+                        <div className={styles.g2} />
+                        <div className={styles.g3} />
+                        <div className={styles.g4} />
+                        <div className={styles.g5} />
+                        <div className={styles.interactive} ref={interBubbleRef}
+                        />
                     </div>
                 </div>
             </div>
-            <div className={styles.gradient_bg}>
-                <svg xmlns="http://www.w3.org/2000/svg">
-                    <defs>
-                        <filter id="goo">
-                            <feGaussianBlur in="SourceGraphic" stdDeviation={10} result="blur" />
-                            <feColorMatrix
-                                in="blur"
-                                mode="matrix"
-                                values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -8"
-                                result="goo"
-                            />
-                            <feBlend in="SourceGraphic" in2="goo" />
-                        </filter>
-                    </defs>
-                </svg>
-                <div className={styles.gradients_container}>
-                    <div className={styles.g1} />
-                    <div className={styles.g2} />
-                    <div className={styles.g3} />
-                    <div className={styles.g4} />
-                    <div className={styles.g5} />
-                    <div className={styles.interactive} ref={interBubbleRef}
-                    />
-                </div>
-            </div>
-        </div>
+            : <Navigate to='/mafia/profile' state={{ from: locaion }} replace />
     )
 }
 
