07e4f4ed498af79ed595812f91fc08e0000975ab
JavadH0sseini <sr.javadhosseini@gmail.com>
2024-03-07 13:41:12 +0330

    only when user is on mobile show the orientation warning

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -3,25 +3,55 @@ import { useSelector } from 'react-redux'
 import { ToastContainer } from 'react-toastify'
 import useScreenOrientation from '../hooks/useScreenOrientation'
 import { AiOutlineRotateLeft } from 'react-icons/ai'
+import { useEffect, useState } from 'react'
 
 
 const AuthLayout = () => {
     const globalUser = useSelector(state => state.userReducer)
     const locaion = useLocation()
     const orientation = useScreenOrientation();
+    const [isMobile, setIsMobile] = useState(false)
+    const [showWarning, setShowWarning] = useState(false)
+
+    useEffect(() => {
+        setIsMobile(detectMob())
+    }, [])
+
+
+    function detectMob() {
+        const toMatch = [
+            /Android/i,
+            /webOS/i,
+            /iPhone/i,
+            /iPad/i,
+            /iPod/i,
+            /BlackBerry/i,
+            /Windows Phone/i
+        ];
+
+        return toMatch.some((toMatchItem) => {
+            return navigator.userAgent.match(toMatchItem);
+        });
+    }
+
+    useEffect(() => {
+        if (isMobile && orientation !== 0)
+            setShowWarning(true)
+        else setShowWarning(false)
+    }, [isMobile, orientation])
 
     return (
         !globalUser.isLoggedIn ?
-            orientation === 0 ?
-                <>
-                    <Outlet />
-                    <ToastContainer position="bottom-center" autoClose={3000} hideProgressBar newestOnTop={false} closeOnClick rtl pauseOnFocusLoss draggable pauseOnHover theme="dark" />
-                </>
-                :
+            showWarning ?
                 <div className='w-full h-screen flex flex-col gap-2 justify-center items-center'>
                     <AiOutlineRotateLeft size={120} />
                     <p>لطفا گوشی خود را عمودی نگه دارید</p>
                 </div>
+                :
+                <>
+                    <Outlet />
+                    <ToastContainer position="bottom-center" autoClose={3000} hideProgressBar newestOnTop={false} closeOnClick rtl pauseOnFocusLoss draggable pauseOnHover theme="dark" />
+                </>
             : <Navigate to='/mafia/profile' state={{ from: locaion }} replace />
     )
 }
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -4,7 +4,7 @@ import Header from '../components/Header'
 import Navbar from '../components/Navbar'
 import { ToastContainer } from 'react-toastify'
 import useScreenOrientation from '../hooks/useScreenOrientation'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { AiOutlineRotateLeft } from "react-icons/ai";
 
 
@@ -12,20 +12,49 @@ const MainLayout = () => {
     const globalUser = useSelector(state => state.userReducer)
     const orientation = useScreenOrientation();
     const locaion = useLocation()
+    const [isMobile, setIsMobile] = useState(false)
+    const [showWarning, setShowWarning] = useState(false)
+
+    useEffect(() => {
+        setIsMobile(detectMob())
+    }, [])
+
+
+    function detectMob() {
+        const toMatch = [
+            /Android/i,
+            /webOS/i,
+            /iPhone/i,
+            /iPad/i,
+            /iPod/i,
+            /BlackBerry/i,
+            /Windows Phone/i
+        ];
+
+        return toMatch.some((toMatchItem) => {
+            return navigator.userAgent.match(toMatchItem);
+        });
+    }
+
+    useEffect(() => {
+        if (isMobile && orientation !== 0)
+            setShowWarning(true)
+        else setShowWarning(false)
+    }, [isMobile, orientation])
 
     return (
         globalUser.isLoggedIn ?
-            orientation === 0 ?
+            showWarning ?
+                <div className='w-full h-screen flex flex-col gap-2 justify-center items-center'>
+                    <AiOutlineRotateLeft size={120} />
+                    <p>لطفا گوشی خود را عمودی نگه دارید</p>
+                </div>
+                :
                 <div className='overflow-hidden font-sans min-h-screen relative px-4 py-[88px]'>
                     <Header />
                     <Outlet />
                     <ToastContainer position="bottom-center" autoClose={3000} hideProgressBar newestOnTop={false} closeOnClick rtl pauseOnFocusLoss draggable pauseOnHover theme="dark" />
                     <Navbar />
-                </div>
-                :
-                <div className='w-full h-screen flex flex-col gap-2 justify-center items-center'>
-                    <AiOutlineRotateLeft size={120} />
-                    <p>لطفا گوشی خود را عمودی نگه دارید</p>
                 </div>
             : <Navigate to='/mafia/login' state={{ from: locaion }} replace />
 
